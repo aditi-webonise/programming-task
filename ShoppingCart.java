e3193a42382578a13d79797ffefefb5d0616ee01
Aditi Mantri <aditi.mantri@webonise.com>
2017-11-06 20:40:09 +0530

    Update ShoppingCart with morw functionality

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,31 +1,58 @@
 import java.util.Scanner;
-class ShoppingCart{
-	public static int colgate_price= 10;
-	public static int closeup_price= 15;
-	public static int no_of_colgate= 0;
-	public static int no_of_closeup= 0;
-	public static int total_colgate;
-	public static int total_closeup;
+class ShoppingCart
+{
+	public static int colgatePrice= 10;
+	public static int closeupPrice= 15;
+	public static int noOfColgate= 0;
+	public static int noOfCloseup= 0;
+	public static int totalColgate;
+	public static int totalCloseup;
 	public static int sum=0;
-	public static String colgate, closeup; 
+	public static String colgate, closeup;
+	static int offer(int noOfColgate){
+		int free = noOfColgate/3;
+		int usual = noOfColgate % 3;
+		return free * 20 + usual * 10;
+	}
+	static void addColgate(){
+			System.out.println("Enter the number of Colgate");
+			Scanner in = new Scanner(System.in);
+			noOfColgate = noOfColgate + in.nextInt(); 
+    }
+	static void addCloseup(){
+		System.out.println("Enter the number of Close Up");
+		Scanner in = new Scanner(System.in);
+		noOfCloseup = noOfCloseup + in.nextInt();
+		
+	}
+	static void removeColgate(){
+		System.out.println("Enter the number of Colgate");
+		Scanner in = new Scanner(System.in);
+		noOfColgate = noOfColgate - in.nextInt();
+		
+	}
+	static void removeCloseup(){
+		System.out.println("Enter the number of Close Up");
+		Scanner in = new Scanner(System.in);
+		noOfCloseup = noOfCloseup - in.nextInt();
+		
+	} 
 	public static void main(String args[])
 	{	
 		System.out.println("Welcome!! We have two products Colgate and Close Up");
-		System.out.println("Price of Colgate is "+ colgate_price);
-		System.out.println("Price of Close Up is "+ closeup_price);
-		while(true)
-		{
+		System.out.println("Price of Colgate is "+ colgatePrice);
+		System.out.println("Price of Close Up is "+ closeupPrice);
+		while(true){
 			System.out.println("Do you want to continue Shopping?");
 			Scanner scanner = new Scanner(System.in);
 			String shop = scanner.nextLine();
-			if (shop.equals("y"))
-			{	System.out.println("Enter your choice\n 1. Add Colgate to your cart\n 2. Add Close Up to your cart\n 3. Remove Colgate from your cart\n 4. Remove Close Up from your cart");
+			if (shop.equals("y")){	
+				System.out.println("Enter your choice\n 1. Add Colgate to your cart\n 2. Add Close Up to your cart\n 3. Remove Colgate from your cart\n 4. Remove Close Up from your cart");
 				Scanner scannera = new Scanner(System.in);
 				int option = scannera.nextInt();	
 				
 				switch(option){
-	 
-					case 1:addColgate();
+	 				case 1:addColgate();
 					break;
 					case 2:addCloseup();
 					break;
@@ -34,67 +61,25 @@ class ShoppingCart{
 					case 4:removeCloseup();
 					default: break;
 				}				
-			}
-			else{
+			}else{
 				System.out.println("Do you want to use the available offer :BUY 2 GET 1 FREE");
 					Scanner scanner5 = new Scanner(System.in);
 					String offer = scanner5.nextLine();
-					if(offer.equals("y"))
-					{
-						total_colgate= offer(no_of_colgate);
-											
+					if(offer.equals("y")){
+						totalColgate= offer(noOfColgate);
+					}else{
+						totalColgate= noOfColgate * colgatePrice;	
 					}
-					else
-					{
-						total_colgate= no_of_colgate * colgate_price;
-						
-					}
-					total_closeup = no_of_closeup * closeup_price;
-					sum= total_colgate + total_closeup ;
+					totalCloseup = noOfCloseup * closeupPrice;
+					sum= totalColgate + totalCloseup ;
 					System.out.println("Total amount to be paid is "+sum);
 					System.out.println("Thanks for visiting!!");
 					break;
 			   }
-    									
-		}
+    	}
 		
 	}
-	static int offer(int no_of_colgate)
-	{
-		int free = no_of_colgate/3;
-		int usual = no_of_colgate % 3;
-		return free * 20 + usual * 10;
-	}
-	static void addColgate()
-	{
-			System.out.println("Enter the number of Colgate");
-			Scanner in = new Scanner(System.in);
-			no_of_colgate = no_of_colgate + in.nextInt();
-        }
-	static void addCloseup()
-	{
-		
-		System.out.println("Enter the number of Close Up");
-		Scanner in = new Scanner(System.in);
-		no_of_closeup = no_of_closeup + in.nextInt();
-		
-	}
-	static void removeColgate()
-	{
-		
-		System.out.println("Enter the number of Colgate");
-		Scanner in = new Scanner(System.in);
-		no_of_colgate = no_of_colgate - in.nextInt();
-		
-	}
-	static void removeCloseup()
-	{
-		
-		System.out.println("Enter the number of Close Up");
-		Scanner in = new Scanner(System.in);
-		no_of_closeup = no_of_closeup - in.nextInt();
-		
-	}
+	
 	
 }
 
